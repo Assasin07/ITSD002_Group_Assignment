3606ede1bdaf379fb1a881606b6cd3b56419b9df
Hot945 <hot945@outlook.com>
2024-10-31 00:26:51 +0800

    test

--- .venv/Student.py
+++ .venv/Student.py
@@ -2,6 +2,9 @@
 #TO INSTALL THESE, INPUT THE FOLLOWING COMMAND "pip install pandas" AND "pip install openpyxl" SEPERATELY IN YOUR TERMINAL
 import pandas as pd
 import random
+from datetime import datetime
+
+path = "C:/Users/Hot945/Desktop/info.xlsx"
 
 #Introduce Sequential Search function
 def sequential_search(arr, x):
@@ -16,7 +19,7 @@ def generate_num():
 
 #Introduce a funtion to transform Excel column data into Python list
 def get_column_list(sheet_name_input, column_name_input):
-    df = pd.read_excel("C:/Users/Hot945/Desktop/info.xlsx", sheet_name=sheet_name_input)
+    df = pd.read_excel(path, sheet_name=sheet_name_input)
     return df[column_name_input].tolist()
 
 #Display STUDENT MENU
@@ -39,25 +42,53 @@ if option == "1":
 
     #Judging the number of lockers student had rented
     list2 = get_column_list("lockers", "Student ID")
-    num = 0
-    for i in range(len(list2)):
-        if sequential_search(list2, studentID) != -1:
-            num += 1
+    num = list2.count(studentID)
+
     #When rented lockers < 2
+    print(num)
     if num < 2:
         while True:
-            lockerID = generate_num()
+            rentalID = generate_num()
             list3 = get_column_list("lockers", "Rental ID")
-            non_repeat_check = sequential_search(list3, lockerID)
+            non_repeat_check = sequential_search(list3, rentalID)
 
             if non_repeat_check == -1:
-                print(lockerID)
+                break
+
+        startDate = datetime.now()
+        df = pd.read_excel(path, sheet_name="lockers", )
+        valid_range = df["Locker ID"].notna().sum()
+
+        for index, value in df["Status"].items():
+            if index > valid_range:
+                break
+
+            if value not in ["Suspended", "Used"]:
+                df.at[index, "Rental ID"] = rentalID
+                df.at[index, "Student ID"] = studentID
+                df.at[index, "Status"] = "Used"
+                df["Start Date"] = df["Start Date"].astype(str)
+                df.at[index, "Start Date"] = pd.to_datetime(startDate).strftime('%d-%b-%y')
+                df['Start Date'] = df['Start Date'].replace('nan', '')
+
+                lockerID = df.at[index, "Locker ID"]
+                location = df.at[index, "Location"]
+
+                with pd.ExcelWriter(path, engine='openpyxl', mode='a', if_sheet_exists='replace') as writer:
+                    df.to_excel(writer, sheet_name="lockers", index=False)
+
+                print("-" * 50, "\n")
+                print("Locker allocated:", lockerID, " ", "Location:", location, "\n")
+                print("Rental ID:", rentalID, "\n")
+                print("Start date:", startDate.strftime("%d-%b-%y"), "\n")
+                print("$2 is charge to your account.")
+                print("-" * 50)
                 break
 
     #When rented lockers = 2
     else:
         print("Sorry, you are already renting 2 lockers.")
-        df = pd.read_excel("C:/Users/Hot945/Desktop/info.xlsx", sheet_name="lockers", )
+        df = pd.read_excel(path, sheet_name="lockers", )
         target = df[df["Student ID"] == studentID]
         for index, row in target.iterrows():
             lockerID = row["Locker ID"]
@@ -68,7 +99,7 @@ if option == "1":
             print("-" * 50, "\n")
             print("Locker allocated:", lockerID, " ", "Location:", location, "\n")
             print("Rental ID:", rentalID_int, "\n")
-            print("Start date:", startDate.strftime("%d-%b-%y"), "\n")
+            print("Start date:", startDate, "\n")
             print("-" * 50)
 
 
